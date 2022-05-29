c79c4382f69916288c6bbfb8f228dfc70481daa8
ttasjwi <ttasjwi920@gmail.com>
2022-05-29 23:59:03 +0900

    [jaypedia/airbnb#14] Refactor: BookingGuests, BookingPeriod에서 생성자 수동 정의
    - Lombok을 사용한 생성자를 수동으로 정의함.
    - 매개변수의 순서가 바뀔 경우 오류를 야기할 수 있음.
    - 생성자에서 매개변수의 순서를 기본적으로 명시해두는 것이 좋다.

--- BE/src/main/java/codesquad/airbnb/booking/domain/BookingGuests.java
+++ BE/src/main/java/codesquad/airbnb/booking/domain/BookingGuests.java
@@ -7,13 +7,19 @@ import javax.persistence.Embeddable;
 @Embeddable
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PUBLIC)
 @EqualsAndHashCode
 public class BookingGuests {
 
     private int adultCount;
     private int kidCount;
     private int infantCount;
+
+    @Builder(access = AccessLevel.PUBLIC)
+    private BookingGuests(int adultCount, int kidCount, int infantCount) {
+        this.adultCount = adultCount;
+        this.kidCount = kidCount;
+        this.infantCount = infantCount;
+    }
 
     public int totalGuestCount() {
         return adultCount + kidCount + infantCount;
