738f9c20d8274365bf95b46bb248646a733dd39a
Millie <jay.lee.2280@gmail.com>
2022-05-31 10:32:30 +0900

    [jaypedia/airbnb#16] Feat: react-router-dom을 활용한 routing 구현
    - Home, SearchResult, NotFound 페이지 라우팅 구현
    
    Co-authored-by: Oliver <jthw1005@users.noreply.github.com>

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,3 +1,22 @@
-export default function App() {
-  return <div>sample</div>;
-}
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Layout from '@/layout';
+import Home from '@/pages/Home';
+import SearchResult from '@/pages/SearchResult';
+import NotFound from '@/pages/NotFound';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="search-result" element={<SearchResult />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
