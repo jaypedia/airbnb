928ff5775ddbd72c3a4d2a04f22231239e2b06d8
ttasjwi <ttasjwi920@gmail.com>
2022-05-31 11:29:04 +0900

    [jaypedia/airbnb#22] Feat: AccommodationCommandService 정의
    - 숙소 엔티티 관련 상태 변경이 발생되는 AccommodationCommandService 정의

---
+++ BE/src/main/java/codesquad/airbnb/accommodation/service/AccommodationCommandService.java
@@ -0,0 +1,12 @@
+package codesquad.airbnb.accommodation.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AccommodationCommandService {
+
+}
