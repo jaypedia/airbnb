12b6dd9e2e0dfcf365c8872a02f1feb94f472983
Millie <jay.lee.2280@gmail.com>
2022-06-01 03:30:05 +0900

    [jaypedia/airbnb#2] Build: webpack 환경 변수 설정
    - production 모드일 때 basename에 airbnb를 설정

--- FE/webpack.config.js
+++ FE/webpack.config.js
@@ -7,83 +7,93 @@ const mode = process.env.NODE_ENV || 'development';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
-module.exports = {
-  mode,
+module.exports = env => {
+  const { prod } = env;
 
-  entry: {
-    main: path.join(__dirname, 'src', 'index.tsx'),
-  },
+  const BASE_NAME = prod ? 'airbnb' : '';
+  const definePlugin = new webpack.DefinePlugin({
+    BASE_NAME: JSON.stringify(BASE_NAME),
+  });
 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+  return {
+    mode,
+
+    entry: {
+      main: path.join(__dirname, 'src', 'index.tsx'),
     },
-    modules: ['node_modules'],
-    extensions: ['.js', '.ts', '.jsx', '.tsx'],
-  },
 
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: '> 0.25%, not dead',
-                  useBuiltIns: 'usage',
-                  corejs: { version: 3, proposals: true },
-                },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+      modules: ['node_modules'],
+      extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: '> 0.25%, not dead',
+                    useBuiltIns: 'usage',
+                    corejs: { version: 3, proposals: true },
+                  },
+                ],
+                '@babel/preset-react',
+                '@babel/preset-typescript',
               ],
-              '@babel/preset-react',
-              '@babel/preset-typescript',
-            ],
-            plugins: ['react-refresh/babel', 'styled-components'],
+              plugins: ['react-refresh/babel', 'styled-components'],
+            },
           },
         },
-      },
-      {
-        test: /\.(sc|c|sa)ss$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(jpe?g|png|gif|mp3|svg|webg)$/,
-        use: ['file-loader'],
-      },
+        {
+          test: /\.(sc|c|sa)ss$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(jpe?g|png|gif|mp3|svg|webg)$/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({ template: path.join(__dirname, 'public', 'index.html') }),
+      new ReactRefreshWebpackPlugin(),
+      new webpack.ProgressPlugin(),
+      new webpack.ProvidePlugin({ React: 'react' }),
+      definePlugin,
     ],
-  },
 
-  plugins: [
-    new HtmlWebpackPlugin({ template: path.join(__dirname, 'public', 'index.html') }),
-    new ReactRefreshWebpackPlugin(),
-    new webpack.ProgressPlugin(),
-    new webpack.ProvidePlugin({ React: 'react' }),
-  ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
 
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-
-  devServer: {
-    client: {
-      overlay: {
-        errors: true,
-        warnings: false,
+    devServer: {
+      client: {
+        overlay: {
+          errors: true,
+          warnings: false,
+        },
+        progress: true,
       },
-      progress: true,
+      compress: true,
+      historyApiFallback: true,
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      open: true,
+      port: PORT,
     },
-    compress: true,
-    historyApiFallback: true,
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    open: true,
-    port: PORT,
-  },
+  };
 };
