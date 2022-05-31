2274729648d68567cafce46564abf1a76a7c2f26
ttasjwi <ttasjwi920@gmail.com>
2022-05-31 12:03:19 +0900

    [jaypedia/airbnb#25] Feat: url 요청이 들어왔을 때 숙소가격통계 api를 응답하도록 함
    - "/api/accommodations/prices"로 요청했을 때 숙소가격통계 샘플 api를 응답하도록 함

--- BE/src/main/java/codesquad/airbnb/accommodation/web/AccommodationController.java
+++ BE/src/main/java/codesquad/airbnb/accommodation/web/AccommodationController.java
@@ -2,7 +2,9 @@ package codesquad.airbnb.accommodation.web;
 
 import codesquad.airbnb.accommodation.service.AccommodationCommandService;
 import codesquad.airbnb.accommodation.service.AccommodationQueryService;
+import codesquad.airbnb.accommodation.web.dto.AccommodationPricesResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +16,9 @@ public class AccommodationController {
     private final AccommodationCommandService accomodationCommandService;
     private final AccommodationQueryService accommodationQueryService;
 
+    //TODO: 가격분포 + 숙소 가격 평균
+    @GetMapping("/prices")
+    public AccommodationPricesResponse priceStatistic() {
+        return AccommodationPricesResponse.sampleApi();
+    }
 }
