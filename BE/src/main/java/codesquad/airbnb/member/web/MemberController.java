95f30bed61ef04266124e4b82bc28bc9866b65c8
ttasjwi <ttasjwi920@gmail.com>
2022-06-02 16:37:43 +0900

    [jaypedia/airbnb#46] Feat: 회원가입, 회원조회 요청에 대해 응답하도록 함
    - MemberController에 회원가입, 회원조회 메서드 추가

--- BE/src/main/java/codesquad/airbnb/member/web/MemberController.java
+++ BE/src/main/java/codesquad/airbnb/member/web/MemberController.java
@@ -1,11 +1,16 @@
 package codesquad.airbnb.member.web;
 
+import codesquad.airbnb.member.domain.Member;
 import codesquad.airbnb.member.service.MemberCommandService;
 import codesquad.airbnb.member.service.MemberQueryService;
+import codesquad.airbnb.member.web.dto.MemberDetailResponse;
+import codesquad.airbnb.member.web.dto.MemberJoinRequest;
+import codesquad.airbnb.member.web.dto.MemberJoinResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
@@ -13,5 +18,20 @@ public class MemberController {
 
     private final MemberCommandService memberCommandService;
     private final MemberQueryService memberQueryService;
+
+    @PostMapping
+    public MemberJoinResponse joinMember(@RequestBody MemberJoinRequest memberJoinRequest) {
+        log.info("Member Join Request = {}", memberJoinRequest);
+        Member member = memberJoinRequest.toEntity();
+        Long memberId = memberCommandService.join(member);
+
+        return new MemberJoinResponse(memberId);
+    }
+
+    @GetMapping("/{id}")
+    public MemberDetailResponse memberDetail(@PathVariable Long id) {
+        Member findMember = memberQueryService.findById(id);
+        return MemberDetailResponse.create(findMember);
+    }
 
 }
