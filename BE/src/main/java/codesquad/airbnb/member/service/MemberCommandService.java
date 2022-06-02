29eb32569b831ec24828320f0afbcaff7cce4016
ttasjwi <ttasjwi920@gmail.com>
2022-06-02 16:24:13 +0900

    [jaypedia/airbnb#46] Feat: MemberCommandService에 회원 가입 기능 추가
    - join 메서드 : 회원가입
    - 중복체크 : 중복된 이름, 중복된 gitHubId가 포함되어 있으면 예외를 던진다.

--- BE/src/main/java/codesquad/airbnb/member/service/MemberCommandService.java
+++ BE/src/main/java/codesquad/airbnb/member/service/MemberCommandService.java
@@ -1,15 +1,46 @@
 package codesquad.airbnb.member.service;
 
+import codesquad.airbnb.exception.DuplicateGitHubIdException;
+import codesquad.airbnb.exception.DuplicateMemberNameException;
+import codesquad.airbnb.member.domain.Member;
 import codesquad.airbnb.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class MemberCommandService {
 
     private final MemberRepository memberRepository;
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        log.info("join Member = {}", member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        validateDuplicateMemberName(member.getName());
+        validateDuplicateGitHubId(member.getGitHubId());
+    }
+
+    private void validateDuplicateMemberName(String memberName) {
+        memberRepository.findByName(memberName)
+                .ifPresent(duplicateMember -> {
+                    throw new DuplicateMemberNameException();
+                });
+    }
+
+    private void validateDuplicateGitHubId(String gitHubId) {
+        memberRepository.findByGitHubId(gitHubId)
+                .ifPresent(duplicateMember -> {
+                    throw new DuplicateGitHubIdException();
+                });
+    }
 
 }
