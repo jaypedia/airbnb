5b4a6c6dff1d84ed576a8ec5a26bb61708308bc5
ttasjwi <ttasjwi920@gmail.com>
2022-06-03 13:25:15 +0900

    [jaypedia/airbnb#57] Feat: 컨트롤러단에서 요청을 받아 숙소 등록, 식별자 조회를 수행하고 응답할 수 있도록 함

--- BE/src/main/java/codesquad/airbnb/accommodation/web/AccommodationController.java
+++ BE/src/main/java/codesquad/airbnb/accommodation/web/AccommodationController.java
@@ -1,24 +1,40 @@
 package codesquad.airbnb.accommodation.web;
 
+import codesquad.airbnb.accommodation.domain.Accommodation;
 import codesquad.airbnb.accommodation.service.AccommodationCommandService;
 import codesquad.airbnb.accommodation.service.AccommodationQueryService;
+import codesquad.airbnb.accommodation.web.dto.AccommodationDetailResponse;
 import codesquad.airbnb.accommodation.web.dto.AccommodationPricesResponse;
+import codesquad.airbnb.accommodation.web.dto.AccommodationRegisterResponse;
+import codesquad.airbnb.accommodation.web.dto.AccommodationSaveRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/accommodations")
 public class AccommodationController {
 
-    private final AccommodationCommandService accomodationCommandService;
+    private final AccommodationCommandService accommodationCommandService;
     private final AccommodationQueryService accommodationQueryService;
 
-    //TODO: 가격분포 + 숙소 가격 평균
+    @PostMapping
+    public AccommodationRegisterResponse register(@RequestBody AccommodationSaveRequest request) {
+        Accommodation accommodation = request.toEntity();
+        Long accommodationId = accommodationCommandService.register(accommodation);
+        return new AccommodationRegisterResponse(accommodationId);
+    }
+
+    @GetMapping("/{id}")
+    public AccommodationDetailResponse accommodationDetail(@PathVariable Long id) {
+        Accommodation accommodation = accommodationQueryService.findById(id);
+        return AccommodationDetailResponse.create(accommodation);
+    }
+
+    //TODO: 가격 목록 가져오기
     @GetMapping("/prices")
     public AccommodationPricesResponse priceStatistic() {
+        // TODO : AccommodationQueryService에, 가격들 목록을 가져오는 메서드 구현
         return AccommodationPricesResponse.sampleApi();
     }
 }
