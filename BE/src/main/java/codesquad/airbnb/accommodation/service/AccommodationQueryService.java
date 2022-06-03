2a2de068ba395b4e8f3e97b79802d41cb8692132
ttasjwi <ttasjwi920@gmail.com>
2022-06-03 13:24:30 +0900

    [jaypedia/airbnb#57] Feat: 숙소 조회 Service 메서드 추가

--- BE/src/main/java/codesquad/airbnb/accommodation/service/AccommodationQueryService.java
+++ BE/src/main/java/codesquad/airbnb/accommodation/service/AccommodationQueryService.java
@@ -1,6 +1,8 @@
 package codesquad.airbnb.accommodation.service;
 
+import codesquad.airbnb.accommodation.domain.Accommodation;
 import codesquad.airbnb.accommodation.repository.AccommodationRepository;
+import codesquad.airbnb.exception.AccommodationNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,4 +14,11 @@ public class AccommodationQueryService {
 
     private final AccommodationRepository accommodationRepository;
 
+    /**
+     * id로 숙소 조회
+     */
+    public Accommodation findById(Long id) {
+        return accommodationRepository.findById(id)
+                .orElseThrow(() -> new AccommodationNotFoundException());
+    }
 }
