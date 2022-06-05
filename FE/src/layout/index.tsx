e806731d8c85b0a1a8bd148eebedf7c30320c347
Millie <jay.lee.2280@gmail.com>
2022-06-06 00:39:31 +0900

    [jaypedia/airbnb#16] Refactor: Layout 컴포넌트 eslint, prettier 적용

--- FE/src/layout/index.tsx
+++ FE/src/layout/index.tsx
@@ -1,16 +1,16 @@
 import { Outlet } from 'react-router-dom';
 
-import Header from '@/components/Header';
-import { SearchProvider } from '@/context/SearchProvider';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import { SearchProvider } from '@/context/SearchProvider';
 
 const Layout = () => {
   return (
-    <SearchProvider>
-      <Header />
-      <Outlet />
-    </SearchProvider>
+    <>
+      <SearchProvider>
+        <Header />
+        <Outlet />
+      </SearchProvider>
       <Footer />
     </>
   );
