46bbd96fe061e63985ba92d5c9eeca767d3904a2
Millie <jay.lee.2280@gmail.com>
2022-06-08 02:04:50 +0900

    [jaypedia/airbnb#69] Feat: DateCell 컴포넌트 내부에 DatePicker 컴포넌트 삽입
    - 날짜가 없는 빈칸 DateCell 요소는 cursor: auto 처리

--- FE/src/components/Calendar/DateCell/DateCell.style.ts
+++ FE/src/components/Calendar/DateCell/DateCell.style.ts
@@ -1,22 +1,41 @@
 import styled from 'styled-components';
 
+import { theme } from '@/styles/theme';
+
+const setCursor = (isPast, isBlank) => {
+  if (isBlank) {
+    return 'auto';
+  }
+  if (isPast) {
+    return 'not-allowed';
+  }
+  return 'pointer';
+};
+
+const checkInDateStyle = `
+  background-color: ${theme.color.grey6};
+  border-top-left-radius:50%;
+  border-bottom-left-radius:50%;
+`;
+
+const checkOutDateStyle = `
+  background-color: ${theme.color.grey6};
+  border-top-right-radius:50%;
+  border-bottom-right-radius:50%;
+`;
+
 const DateCell = styled.td`
   width: 48px;
   height: 47px;
   text-align: center;
   font-size: ${({ theme: { fontSize } }) => fontSize.xSmall};
-  cursor: ${({ isPast }) => (isPast ? 'not-allowed' : 'pointer')};
+  cursor: ${({ isPast, isBlank }) => setCursor(isPast, isBlank)};
   color: ${({ theme: { color }, isPast }) => (isPast ? color.grey4 : color.black)};
-  border-radius: 50%;
-
-  ${({ isPast }) =>
-    !isPast &&
-    `
-  :hover {
-    background-color: black;
-    color: white;
-  }
-  `}
+  border: 0;
+  padding: 0;
+  background-color: ${({ theme: { color } }) => color.white};
+  ${({ isCheckIn }) => isCheckIn && checkInDateStyle};
+  ${({ isCheckOut }) => isCheckOut && checkOutDateStyle};
 `;
 
 export { DateCell };
--- FE/src/components/Calendar/DateCell/index.tsx
+++ FE/src/components/Calendar/DateCell/index.tsx
@@ -1,18 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import * as S from './DateCell.style';
 
 import { CalendarContext } from '@/components/Calendar';
+import DatePicker from '@/components/Calendar/DatePicker';
+import { useDatePickerState } from '@/context';
+import { checkDateIsPast, getKoreanMonthDateString } from '@/utils/calendar';
 
 const DateCell = ({ date, monthIdx }) => {
   const { year, today } = useContext(CalendarContext);
+  const { checkIn, checkOut } = useDatePickerState();
 
-  const isPast = () => {
-    const thisDate = new Date(year, monthIdx, date);
-    return today > thisDate;
+  const isThisDatePast = checkDateIsPast({ year, monthIdx, date, today });
+  const checkInOutString = getKoreanMonthDateString(date, monthIdx);
+
+  const [isCheckIn, setIsCheckIn] = useState(false);
+  const [isCheckOut, setIsCheckOut] = useState(false);
+
+  const handleDateClick = () => {
+    // TODO: 로직 구현
   };
 
-  return <S.DateCell isPast={isPast()}>{date}</S.DateCell>;
+  return (
+    <S.DateCell
+      isPast={isThisDatePast}
+      isBlank={!date}
+      onClick={handleDateClick}
+      isCheckIn={isCheckIn}
+      isCheckOut={isCheckOut}
+    >
+      {!isThisDatePast && date ? <DatePicker date={date} monthIdx={monthIdx} /> : date}
+    </S.DateCell>
+  );
 };
 
 export default DateCell;
