5f8e59880b96971e12bb05e6808e9855d2f69d76
jthw1005 <skl0105@naver.com>
2022-06-09 17:29:43 +0900

    [jaypedia/airbnb#53] Feat: Range Slider 내 평균 가격 표기

--- FE/src/utils/rangeSlider.ts
+++ FE/src/utils/rangeSlider.ts
@@ -6,7 +6,7 @@ function getRangeArrInd(num, min, d) {
   const result = Math.floor((num - min) / d);
   return result;
 }
-function dividePrices(priceData) {
+function getRangeSliderValues(priceData) {
   const NUM_OF_RANGE_BARS = 29;
   const RANGE_PRICE_MAX = 0.05;
   const rangePriceCnt = priceData.length;
@@ -15,25 +15,27 @@ function dividePrices(priceData) {
   const min = priceData[0];
   const max = priceData[rangePriceCnt - priceMaxBoundary];
 
+  const rangeAvg = Math.floor(priceData.reduce((result, curr) => result + curr) / rangePriceCnt);
+
   const rangeMin = Math.floor(min / 10 ** (getNumDigit(min) - 1)) * 10 ** (getNumDigit(min) - 1);
   const rangeMax = Math.floor(max / 10 ** (getNumDigit(max) - 1)) * 10 ** (getNumDigit(max) - 1);
-  const rangeGap = Math.round((rangeMax - rangeMin) / NUM_OF_RANGE_BARS);
+  const rangeGap = Math.floor((rangeMax - rangeMin) / NUM_OF_RANGE_BARS);
 
-  const rangeObj = {};
+  const graphBarCntObj = {};
   for (let i = 0; i < NUM_OF_RANGE_BARS + 1; i++) {
-    rangeObj[i] = 0;
+    graphBarCntObj[i] = 0;
   }
 
   priceData.forEach(el => {
     const idx = getRangeArrInd(el, rangeMin, rangeGap);
     if (idx >= NUM_OF_RANGE_BARS) {
-      rangeObj[NUM_OF_RANGE_BARS]++;
+      graphBarCntObj[NUM_OF_RANGE_BARS]++;
     } else {
-      rangeObj[idx]++;
+      graphBarCntObj[idx]++;
     }
   });
 
-  return { rangeObj, rangeMin, rangeMax, rangeGap };
+  return { graphBarCntObj, rangeMin, rangeMax, rangeGap, rangeAvg };
 }
 
-export default dividePrices;
+export default getRangeSliderValues;
